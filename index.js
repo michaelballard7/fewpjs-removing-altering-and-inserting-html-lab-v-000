58f20fb6fd72c08b891309d141b139ee2408564d
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 14:59:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+let body = document.querySelector('body')
+
+
+let delEl = document.querySelector('#main')
+
+delEl.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent = 'YOUR-NAME is the champion'
+
+body.appendChild(newHeader)

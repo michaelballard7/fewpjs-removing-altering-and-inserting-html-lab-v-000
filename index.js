9a8757b1980645c2feada7936a3a60990629b745
Michael Ballard <ballard.michael9@gmail.com>
2020-11-14 14:58:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // Write your code here!
 
+let body = document.querySelector('body')
+
+
 let delEl = document.querySelector('#main')
 delEl.remove()
 
 let newHeader = document.createElement('h1')
 newHeader.setAttribute('id','victory')
+newHeader.textContent = 'YOUR-NAME is the champion'
+
+
+
+body.appendChild(newHeader)
